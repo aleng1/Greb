d123b3509a0376ed2d6b14cdf52bf8d68f8751ca
A.Halim <abdhalim2002@gmail.com>
2022-05-29 18:42:32 +0800

    Merge origin/master
    Conflicts:
    	OCC5-06/nbproject/project.properties

---
+++ Greb/src/greb/Greb.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package greb;
+
+/**
+ *
+ * @author ABD HALIM
+ */
+public class Greb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
